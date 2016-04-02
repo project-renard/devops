c1e790ea3a5a3eac3eeb7bbcfa1fa5d796016286
chirag <chiragghanshani@gmail.com>
2016-04-02 13:37:39 -0500

    added openssl dev package

--- VagrantFiles/provision.sh
+++ VagrantFiles/provision.sh
@@ -3,3 +3,4 @@ sudo apt-get -y upgrade
 sudo apt-get -y install build-essential vim curl wget libgirepository1.0-dev libgdl-3-5 gobject-introspection libgtk-3-dev
 sudo apt-get -y install gir1.2-gdl-3 libpoppler-glib-dev poppler-utils mupdf-tools git libglib-object-introspection-perl
 sudo apt-get -y install --no-install-recommends glade
+sudo apt-get -y install libssl-dev
