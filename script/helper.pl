4473aaab2f83da6cb19490c5077e1729e77358cf
Zakariyya Mughal <zaki.mughal@gmail.com>
2018-03-15 10:41:00 -0400

    Install unzip and xvfb on Vagrant

--- script/helper.pl
+++ script/helper.pl
@@ -337,6 +337,7 @@ sudo apt-get -y update
 sudo apt-get -y upgrade
 sudo apt-get -y install build-essential vim curl wget libgirepository1.0-dev libgdl-3-5 gobject-introspection libgtk-3-dev
 sudo apt-get -y install gir1.2-gdl-3 libpoppler-glib-dev poppler-utils mupdf-tools git libglib-object-introspection-perl
+sudo apt-get -y install xvfb unzip
 sudo apt-get -y install --no-install-recommends glade
 sudo apt-get -y install libssl-dev
 
